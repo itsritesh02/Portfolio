a3c7d4ca2d014741e5f0b0d0fb5e39184183c2db
itsritesh02 <itsritesh02@gmail.com>
2026-08-28 17:50:09 +0530

    form

--- server/server.js
+++ server/server.js
@@ -2,26 +2,22 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import connectDB from "./config/db.js";
 import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-connectDB();
-
 const allowedOrigins = [
   "http://localhost:5173",
   "https://portfolio-yq6c.vercel.app",
-  "https://portfolio-yq6c-git-main-itsritesh02-9921s-projects.vercel.app",
 ];
 
 app.use(
   cors({
     origin: allowedOrigins,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
